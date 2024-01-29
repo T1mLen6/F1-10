78603a321da1a8c7a349fb4c7f596fe8d560f7e1
Letian Leng <113462284+T1mLen6@users.noreply.github.com>
2024-01-29 00:55:14 -0500

    Lab2_deliverable_1&2_finished

---
+++ sim_ws/build/f1tenth_gym_ros/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/tim/F1_10/sim_ws/install/f1tenth_gym_ros'
---
+++ sim_ws/src/safety_node/scripts/safety_node.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+from math import sqrt
+import rclpy
+from rclpy.node import Node
+
+import numpy as np
+# TODO: include needed ROS msg type headers and libraries
+from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
+from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
+
+
+class SafetyNode(Node):
+    """
+    The class that handles emergency braking.
+    """
+    def __init__(self):
+        super().__init__('safety_node')
+        """
+        One publisher should publish to the /drive topic with a AckermannDriveStamped drive message.
+
+        You should also subscribe to the /scan topic to get the LaserScan messages and
+        the /ego_racecar/odom topic to get the current speed of the vehicle.
+
+        The subscribers should use the provided odom_callback and scan_callback as callback methods
+
+        NOTE that the x component of the linear velocity in odom is the speed
+        """
+        self.speed = 0.
+        # TODO: create ROS subscribers and publishers.
+
+        self.publisher_ = self.create_publisher(AckermannDriveStamped, 'drive', 10)
+
+        self.subscription1 = self.create_subscription(
+            Odometry,
+            'ego_racecar/odom',
+            self.odom_callback,
+            10)
+        
+        self.subscription1  # prevent unused variable warning
+
+        self.subscription2 = self.create_subscription(
+            LaserScan,
+            'scan',
+            self.scan_callback,
+            10)
+        
+        self.subscription2  # prevent unused variable warning
+
+        # self.subscription3 = self.create_subscription(
+        #     AckermannDriveStamped,
+        #     'drive',
+        #     self.listener_callback,
+        #     10)
+        
+        # self.subscription3  # prevent unused variable warning
+
+
+
+        
+
+    def odom_callback(self, odom_msg):
+        # TODO: update current speed
+        vx = odom_msg.twist.twist.linear.x
+        vy = odom_msg.twist.twist.linear.y
+        self.speed = np.sqrt(vx**2 + vy**2) 
+        #self.get_logger().info('Received speed: %s' % self.speed)
+
+    def scan_callback(self, scan_msg):
+        # TODO: calculate TTC
+        TTC = scan_msg.ranges[540] / self.speed
+
+        #scan_msg.scan.ranges 
+        #print('yeah', scan_msg.angle_min)
+        self.get_logger().info('Received range: %s' % scan_msg.ranges[540])
+        self.get_logger().info('TTC: %s' % TTC)
+        #self.get_logger().info('Received min angle: %s' % scan_msg.angle_increment)
+
+        threshold = 2.0
+        if TTC <= threshold:
+          ADS = AckermannDriveStamped()
+          ADS.drive.speed = 0.0
+          self.publisher_.publish(ADS)
+
+
+        # TODO: publish command to brake
+        #pass
+
+def main(args=None):
+    rclpy.init(args=args)
+    safety_node = SafetyNode()
+    rclpy.spin(safety_node)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    safety_node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
